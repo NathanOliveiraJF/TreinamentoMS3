75c16385a9b007166fa259cedd0a7e96206efd6d
NathanOliveiraJF <nathanoliveira003jf@gmail.com>
2021-02-20 11:55:24 -0300

    example observer pattern implemented

---
+++ Observer.Pattern/Observer/Subject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Observer
+{
+    abstract class Subject
+    {
+        private readonly List<IObserver> _observers = new List<IObserver>();
+
+        public void Attach(IObserver observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void Detach(IObserver observer)
+        {
+            if(_observers.Contains(observer))
+                _observers.Remove(observer);
+        }
+
+        public void Notify()
+        {
+            foreach (var o in _observers)
+                o.Update();
+        }
+    }
+}
