a182307ab286e9ffb7ac7d0dee4bffe081e4af8a
NathanOliveiraJF <nathanoliveira003jf@gmail.com>
2021-02-20 11:59:38 -0300

    update local folder

---
+++ DesignPattern/Observer.Pattern/Observer/ConcreteSubject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Observer
+{
+    class ConcreteSubject : Subject
+    {
+        private string _subjectState;
+
+        public string SubjectState
+        {
+            get { return _subjectState; }
+            set { _subjectState = value; }
+        }
+    }
+}
